050f82cbedf1e269eab647b7971ed884de7850d4
codeWithGz <giustt4@gmail.com>
2026-06-10 20:32:31 -0300

    adicionando sistema de favoritos usando localStorage e animação CSS na estrela

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let templateCard = `
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 p-2">
             <div class="card h-100">
@@ -10,6 +9,15 @@ let templateCard = `
                         {{description}}
                     </p>
                     
+                    <div class="d-flex justify-content-between align-items-center mt-2">
+                        <iconify-icon 
+                            icon="lucide:star" 
+                            class="fs-3 star-fav {{#if isFavorited}}favorited{{/if}}" 
+                            onclick="toggleFavorite({{id}}, this)"
+                            title="Favoritar">
+                        </iconify-icon>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -22,15 +30,44 @@ async function lerProdutos() {
     let data = await response.json();
 
     let contentHtml = "";
+    
+    let favs = getFavoritos();
 
     for (let idx in data.products) {
         let product = data.products[idx];
+        
+        product.isFavorited = favs.includes(product.id);
+        
         contentHtml = contentHtml + template(product);
     }
 
     let elem = document.getElementById("ProductsList");
     elem.innerHTML = contentHtml;
-
 }
+
 lerProdutos();
+
+function getFavoritos() {
+    let favs = localStorage.getItem('favoritos');
+    return favs ? JSON.parse(favs) : [];
+}
+
+function salvarFavoritos(favs) {
+    localStorage.setItem('favoritos', JSON.stringify(favs));
+}
+
+function toggleFavorite(produtoId, iconeHTML) {
+    let favs = getFavoritos();
+    let index = favs.indexOf(produtoId);
+
+    if (index === -1) {
+        favs.push(produtoId);
+        iconeHTML.classList.add('favorited'); 
+    } else {
+        favs.splice(index, 1);
+        iconeHTML.classList.remove('favorited'); 
+    }
+
+    salvarFavoritos(favs);
+}
 
